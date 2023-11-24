7c0fdc38a417ae567373985a6c08d1e7735cc0fd
AnPhung <anphung311002@gmail.com>
2023-11-24 14:05:40 +0700

    complete the client's login page

--- Project/Services/ApplicantRepository.cs
+++ Project/Services/ApplicantRepository.cs
@@ -1,12 +1,25 @@
-﻿using Project.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Data;
 using Project.Services.IRepository;
 
 namespace Project.Services
 {
     public class ApplicantRepository : Repository<Applicant>, IApplicantRepository
     {
+        private readonly ApplicationDbContext _db;
         public ApplicantRepository(ApplicationDbContext db) : base(db)
         {
+            _db = db;
+        }
+
+        public async Task<int?> CheckLogin(string email, string password)
+        {
+            Applicant? applicant = await _db.Applicants.SingleOrDefaultAsync(a => a.Email == email && a.Password == password);
+            if (applicant != null)
+            {
+                return applicant.Id;
+            }
+            return null;
         }
     }
 }
